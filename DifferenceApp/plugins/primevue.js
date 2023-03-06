1880840b31a9c323ca29f665aef539a69fe8f527
Ludovic <khrisdow@protonmail.com>
2023-03-06 10:00:53 +0100

    feat: add primevue component for UI

--- DifferenceApp/plugins/primevue.js
+++ DifferenceApp/plugins/primevue.js
@@ -2,12 +2,17 @@ import {defineNuxtPlugin} from "#app";
 import PrimeVue from "primevue/config";
 import Button from "primevue/button";
 import InputText from "primevue/inputtext";
+import AutoComplete from "primevue/autocomplete";
+import Dropdown from "primevue/dropdown";
 
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, {ripple: true});
     nuxtApp.vueApp.component('Button', Button);
     nuxtApp.vueApp.component('InputText', InputText);
+    nuxtApp.vueApp.component('AutoComplete', AutoComplete);
+    nuxtApp.vueApp.component('Dropdown', Dropdown);
+
 
     //other components that you need
 });
