cc7864513d6a1f7588ce07e5a65f616f0ab7be23
Ludovic <khrisdow@protonmail.com>
2023-03-06 09:51:41 +0100

    feat: Working progresse to add session system

--- DifferenceApp/nuxt.config.ts
+++ DifferenceApp/nuxt.config.ts
@@ -1,34 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import {plugins} from "nitropack/dist/runtime/virtual/plugins";
-import {process} from "unenv/runtime/node/process/_process";
-import {env} from "unenv";
-import {server} from "nitropack/dist/runtime/entries/firebase";
-import axios from "axios";
-import * as cheerio from 'cheerio';
-
+import fetch from "node-fetch";
 export default defineNuxtConfig({
 
-    routeRules: {
-        '/_nuxt/**': {headers: {'access-control-allow-methods': 'GET'}},
-        '/api/v1/**': {cors: true},
-        '/**': {
-            cors: true,
-            headers: {'Access-Control-Allow-Origin': '*', 'access-control-allow-methods': 'GET'}
-        }
-    },
+        modules: ['@sidebase/nuxt-auth'],
 
-    css: [
-        'primevue/resources/themes/soho-dark/theme.css',
-        'primevue/resources/primevue.css',
-        'primeicons/primeicons.css',
-        'primeflex/primeflex.css'
-
-    ],
-    build: {
-        transpile: ['primevue']
+        debug: true,
+        nitro: {
+            logLevel: 1,
         },
+        //auth: { origin: 'http://localhost:3000/' },
+        routeRules: {
+            '/proxy/**': {
+                proxy: {to: 'https://www.jeuxdemots.org/**'},
+
+            },
+
+        },
+
+        css: ['primevue/resources/themes/soho-dark/theme.css',
+            'primevue/resources/primevue.css',
+            'primeicons/primeicons.css',
+            'primeflex/primeflex.css'
+        ],
+
         ssr: true,
-  }
+
+        build: {
+            transpile: ['primevue']
+        },
+    }
 )
 
 
