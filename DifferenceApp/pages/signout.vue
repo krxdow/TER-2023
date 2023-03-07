20127d6946a27c7c40c5877fb6fb670f23cc7006
Ludovic <khrisdow@protonmail.com>
2023-03-07 08:01:57 +0100

    feat: add some code to perform authentication

---
+++ DifferenceApp/pages/signout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    You just signed out! <br>
+    <NuxtLink to="/">
+      <Button label="Home"></Button>
+    </NuxtLink>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {definePageMeta} from '#imports'
+
+
+definePageMeta({auth: false})
+</script>
