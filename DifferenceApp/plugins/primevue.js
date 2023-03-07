3efb1f55636e7bcc22bca5ed79c5531e2a0e0da7
Ludovic <khrisdow@protonmail.com>
2023-03-07 08:13:47 +0100

    feat: add plugin Menubar for primevue css

--- DifferenceApp/plugins/primevue.js
+++ DifferenceApp/plugins/primevue.js
@@ -4,7 +4,7 @@ import Button from "primevue/button";
 import InputText from "primevue/inputtext";
 import AutoComplete from "primevue/autocomplete";
 import Dropdown from "primevue/dropdown";
-
+import Menubar from 'primevue/menubar';
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, {ripple: true});
@@ -12,7 +12,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component('InputText', InputText);
     nuxtApp.vueApp.component('AutoComplete', AutoComplete);
     nuxtApp.vueApp.component('Dropdown', Dropdown);
-
+    nuxtApp.vueApp.component('Menubar', Menubar);
 
     //other components that you need
 });
