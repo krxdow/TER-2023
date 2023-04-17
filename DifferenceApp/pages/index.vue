7955b90f63087eacecd1f82d20397e6f2c6941a2
Ludovic <khrisdow@protonmail.com>
2023-04-17 13:18:42 +0200

    refactor: style page improve

--- DifferenceApp/pages/index.vue
+++ DifferenceApp/pages/index.vue
@@ -1,34 +1,32 @@
 <template>
-
     <div class="flex  flex-column align-items-center justify-content-center   h-screen ">
-        <div class="surface-card  p-4 shadow-2 border-round w-full lg:w-6 flex flex-column justify-content-center ">
-            <div class="text-center mb-5 ">
 
-                <div class="flex flex-column  align-items-center">
-                    <h1>Trouver Les differences !</h1>
-                    <ul class="list-none ">
-                        <li class="">
-                            <NuxtLink to="/demo">
-                                <Button class=" text-8xl p-button-text hover:bg-blue-800 " icon=""
-                                        label="Démo"></Button>
-                            </NuxtLink>
-                        </li>
-                        <li>
-                            <Button class=" text-8xl p-button-text hover:bg-red-800  " icon="" label="Jouer"></Button>
-                        </li>
+        <div class="surface-card  p-4 shadow-2 border-round w-full lg:w-6 flex flex-column align-items-center justify-content-center text-center ">
+            <h1>Trouver Les differences !</h1>
+        <ul class="list-none   ">
+            <li class="">
+                <NuxtLink to="/demo">
+                    <Button class=" text-8xl p-button-text hover:bg-blue-800 " icon="" label="Démo"></Button>
+                </NuxtLink>
+            </li>
+            <li>
+                <Button class=" text-8xl p-button-text hover:bg-red-800  " icon="" label="Jouer"></Button>
+            </li>
 
-      <NuxtLink to="/partycreation">
-        <li>
-          <Button class=" text-6xl p-button-text hover:bg-yellow-800  " icon="" label="Creer une Partie"></Button>
-        </li>
-      </NuxtLink>
-    </ul>
+            <NuxtLink to="/partycreation">
+                <li>
+                    <Button class=" text-6xl p-button-text hover:bg-yellow-800  " icon=""
+                            label="Creer une Partie"></Button>
+                </li>
+            </NuxtLink>
+        </ul>
+    </div>
     </div>
 </template>
 
 <script>
 export default {
-  name: "index"
+    name: "index"
 }
 </script>
 
