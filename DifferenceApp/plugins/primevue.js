863ffc30a5fd0504e51ad7161ef511bc5934c478
Ludovic <khrisdow@protonmail.com>
2023-04-17 05:59:52 +0200

    feat : add Divider style component

--- DifferenceApp/plugins/primevue.js
+++ DifferenceApp/plugins/primevue.js
@@ -1,10 +1,11 @@
-import {defineNuxtPlugin} from "#app";
+
 import PrimeVue from "primevue/config";
 import Button from "primevue/button";
 import InputText from "primevue/inputtext";
 import AutoComplete from "primevue/autocomplete";
 import Dropdown from "primevue/dropdown";
 import Menubar from 'primevue/menubar';
+import Divider from 'primevue/divider';
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, {ripple: true});
@@ -13,6 +14,8 @@ export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component('AutoComplete', AutoComplete);
     nuxtApp.vueApp.component('Dropdown', Dropdown);
     nuxtApp.vueApp.component('Menubar', Menubar);
+    nuxtApp.vueApp.component('Divider', Divider);
+
 
     //other components that you need
 });
