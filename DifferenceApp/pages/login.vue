18c067d053c5d39435c545492b7f46f29df081d2
Ludovic <khrisdow@protonmail.com>
2023-04-17 05:54:03 +0200

    feat : add new feature to register a user

---
+++ DifferenceApp/pages/login.vue
@@ -0,0 +1,124 @@
+<template>
+
+    <div class="flex  flex-column align-items-center justify-content-center   h-screen ">
+
+        <div class="surface-card  p-4 shadow-2 border-round w-full lg:w-6 flex flex-column justify-content-center ">
+            <div class="text-center mb-5 " v-if="show">
+                <!--
+                                <img alt="Image" class="mb-3" height="50" src="images/blocks/logos/hyper.svg">
+                -->
+                <div class="text-900 text-3xl font-medium mb-3">Welcome Back</div>
+                <span class="text-600 font-medium line-height-3">Don't have an account?</span>
+                <a class="font-medium no-underline ml-2 text-blue-500 cursor-pointer"   @click="show = !show;udpateLabel(show)" >Create today!</a>
+            </div>
+            <div class="text-center mb-5 " v-if="!show">
+                <!--
+                                <img alt="Image" class="mb-3" height="50" src="images/blocks/logos/hyper.svg">
+                -->
+                <div class="text-900 text-3xl font-medium mb-3">You have an account?</div>
+                <span class="text-600 font-medium line-height-3 font-medium no-underline ml-2 text-blue-500 cursor-pointer" @click="show = !show;udpateLabel(show)" >Sign in</span>
+            </div>
+
+
+            <form @submit.prevent>
+                <div class="text-center">
+
+                    <label class="block text-900 font-medium mb-2" for="email1">{{ emailLabel }}</label>
+                    <InputText  id="email1" v-model="email" class="w-full mb-3" type="email"/>
+
+
+                    <label v-if="!show" class="block text-900 font-medium mb-2" for="username1">Entrer un Non Utilisateur</label>
+                    <InputText v-if="!show" id="username1" v-model="username" class="w-full mb-3" type="email"/>
+
+                    <label class="block text-900 font-medium mb-2" for="password1">{{ passwordLabel }}</label>
+                    <InputText  id="password1" v-model="password" class="w-full mb-3" :type="inputType" :autocomplete="autocompleteValue" />
+
+
+
+
+                    <div class="flex align-items-center justify-content-between mb-6">
+                        <div class="flex align-items-center">
+                            <!--                        <Checkbox id="rememberme1" class="mr-2"></Checkbox>-->
+                            <label for="rememberme1">Remember me</label>
+                        </div>
+                        <a class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot
+                            password?</a>
+                    </div>
+
+
+                </div>
+            </form>
+            <div v-if="show">
+                <Button class="w-full" label="Sign In"
+                        @click="signIn('credentials', {username,password})"></Button>
+                <Divider  align="center" layout="horizontal"><b>OR</b></Divider>
+                <Button class="w-full"  label="Sign In With Github"
+                        @click="signIn('github',{ callbackUrl: '/' })"></Button>
+            </div>
+
+
+            <div v-if="!show">
+                <Button class="w-full" label="Register"
+                        type="submit" @click="addUsers(username, email, password)">
+                </Button>
+            </div>
+
+        </div>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+defineAppConfig({
+middleware: false,
+})
+
+
+const show = ref(true)
+let emailLabel = ref('');
+let passwordLabel = ref('');
+const password = ref('')
+
+const users = ref(null)
+const {signIn} = useAuth()
+
+    let inputType = ref('password')
+    let autocompleteValue = ref('off')
+
+    const email = ref('')
+    const username = ref('')
+    emailLabel.value = 'Email';
+    passwordLabel.value = 'Password'
+
+
+
+async function addUsers(username, email, password) {
+
+    let addedUser = null
+
+    if(username && email)  {
+        addedUser = await $fetch('/api/users', {
+            method: 'POST',
+            body: {
+                name: username,
+                email: email,
+                password: password
+            }
+        })
+    }
+}
+
+    async function udpateLabel(show) {
+        emailLabel.value = show ? 'Email' : 'Entrer un email';
+        passwordLabel.value = show ? 'Password' : 'Entrer un mot de passe';
+        inputType.value = show ? 'password' : 'text';
+        autocompleteValue.value = show ? 'off' : 'new-password';
+    }
+
+
+
+</script>
+
+<style scoped>
+
+</style>
