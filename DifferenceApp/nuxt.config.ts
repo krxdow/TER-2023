70ec30fc2524bb51c861f77e1fbc7a80928ad5a1
Ludovic <khrisdow@protonmail.com>
2023-04-21 07:48:40 +0200

    feat: add new module @nuxt/devtools' for debugging  and changing auth options

--- DifferenceApp/nuxt.config.ts
+++ DifferenceApp/nuxt.config.ts
@@ -1,44 +1,43 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-
-
 export default defineNuxtConfig({
-        modules: [
-            '@sidebase/nuxt-auth',
-            '@nuxt/devtools',
-        ],
+    modules: [
+        '@sidebase/nuxt-auth',
+        '@nuxt/devtools',
+    ],
 
-        nitro: {
-            logLevel: 1,
+    build: {
+        transpile: ['primevue']
+    },
+
+
+    auth: {
+        origin: 'http://localhost:3000',
+        isEnabled: true,
+    },
+
+    css: [
+        'primevue/resources/themes/soho-dark/theme.css',
+        'primevue/resources/primevue.css',
+        'primeicons/primeicons.css',
+        'primeflex/primeflex.css'
+    ],
+
+    debug: true,
+
+    nitro: {
+        logLevel: 1,
+    },
+
+    routeRules: {
+        '/proxy/**': {
+            proxy: {to: 'https://www.jeuxdemots.org/**'},
         },
 
-        debug: true,
+    },
 
-        auth: {
-            //https://sidebase.io/nuxt-auth/configuration/nuxt-config
-            isEnabled: true,
-            origin: process.env.ORIGIN,
-            basePath: '/api/auth',
-        },
+    typescript: {
+        strict: true
+    },
 
-        routeRules: {
-            '/proxy/**': {
-                proxy: {to: 'https://www.jeuxdemots.org/**'},
-
-            },
-
-        },
-
-        css: [
-            'primevue/resources/themes/soho-dark/theme.css',
-            'primevue/resources/primevue.css',
-            'primeicons/primeicons.css',
-            'primeflex/primeflex.css'
-        ],
-
-        ssr: true,
-
-        build: {
-            transpile: ['primevue']
-        },
-    }
-)
+    devtools: true
+})
