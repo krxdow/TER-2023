23033c323c310de6d5d96dc5872fddb51712ebb0
Ludovic <khrisdow@protonmail.com>
2023-04-25 01:17:32 +0200

    feat: add an JavaScript linter that enables you to enforce a set of style, formatting, and coding standards about Nuxt project

---
+++ DifferenceApp/.eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    "root": true,
+    "parser": "@babel/eslint-parser",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "extends": ["@nuxt/eslint-config"],
+};
