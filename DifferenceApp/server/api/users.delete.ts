b18c94bdb7f04523fab0c515e268991f1f41ad6f
Gabrielle P <114810027+Gaby269@users.noreply.github.com>
2023-04-25 01:04:30 +0200

    Changement en cours sur prisma

--- DifferenceApp/server/api/users.delete.ts
+++ DifferenceApp/server/api/users.delete.ts
@@ -1,31 +1,30 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-
-//api for deleting a user from the database by id
+//API pour supprimer un utilisateur de la base de donnée par l'id
 export default defineEventHandler(async (event) => {
-    console.log("delete user");
-    const body = await readBody(event);
-    const userId = body.id; // Make sure that you have the correct user ID from the request body
-   // const userName = body.name;
+  console.log("delete user");
+  const body = await readBody(event);
+  const userId = body.id_user; // Make sure that you have the correct user ID from the request body
+  // const userName = body.name;
 
-    try {
-        const resp = await prisma.user.delete({
-            where: {
-                id: userId // Pass the correct user ID to delete the user
-            }
-        });
+  try {
+    const resp = await prisma.user.delete({
+      where: {
+        id_user: userId, // Pass the correct user ID to delete the user
+      },
+    });
 
-        return {
-            success: true,
-            message: `User with ID ${userId} has been deleted.`,
-            data: resp
-        };
-    } catch (error) {
-        return {
-            success: false,
-            message: `Failed to delete user with ID ${userId}.`,
-            error: error
-        };
-    }
+    return {
+      success: true,
+      message: `User with ID ${userId} has been deleted.`,
+      data: resp,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: `Failed to delete user with ID ${userId}.`,
+      error: error,
+    };
+  }
 });
